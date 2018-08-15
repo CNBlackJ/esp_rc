7ec2caafb893b4f5abf7f600f2ce578b553a43a3
CNBlackJ <yes.heng@icloud.com>
2018-08-15 12:35:21 +0800

    feat: add joystick

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client', '/demo.html'))
+})
+
+app.get('/joystick', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', '/joystick.html'))
+})
+
+app.use('/assets', express.static(path.join(__dirname, '../client/assets')))
+
+app.listen(3000, () => {
+  console.log('app listening: http://localhost:3000')
+})
